1f86f47eec4eab3d63b7f21e0d8a0e2e01205487
1283j14 <yumazigarude1214@icloud.com>
2025-07-02 10:18:05 +0900

    Create song.py

---
+++ song.py
@@ -0,0 +1,15 @@
+class Song:
+    """楽曲データを管理するクラス"""
+    def __init__(self, title, artist, image_url, spotify_id=None):
+        self.title = title
+        self.artist = artist
+        self.image_url = image_url
+        self.spotify_id = spotify_id
+    
+    def __str__(self):
+        return f"{self.title} - {self.artist}"
+    
+    def __eq__(self, other):
+        if isinstance(other, Song):
+            return self.title == other.title and self.artist == other.artist
+        return False
