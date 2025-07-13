4d5b4b85371417ea3924e7b0f58ccf2ddc2e3c5f
1283j14 <yumazigarude1214@icloud.com>
2025-07-13 23:05:55 +0900

    Update song.py

--- song.py
+++ song.py
@@ -1,10 +1,12 @@
 class Song:
     """楽曲データを管理するクラス"""
-    def __init__(self, title, artist, image_url, spotify_id=None):
+    def __init__(self, title, artist, image_url, spotify_id=None, preview_url=None, spotify_uri=None):
         self.title = title
         self.artist = artist
         self.image_url = image_url
         self.spotify_id = spotify_id
+        self.preview_url = preview_url
+        self.spotify_uri = spotify_uri
     
     def __str__(self):
         return f"{self.title} - {self.artist}"
